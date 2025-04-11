6b3ac06f4e411a70bbd136ba687d3530f9552559
Michiel <97068201@st.deltion.nl>
2025-04-11 13:34:55 +0200

    michiel
    EN NU ECHT VAN AF BLIJVEN NU WERKT EN JE DASHBOARD EN JE PROJECTDETAIL PAGINA

--- Website/routes/web.php
+++ Website/routes/web.php
@@ -14,6 +14,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('project.destroy');
+    Route::get('projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
     Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
     Route::get('/projects/{projects}', [ProjectController::class, 'update'])->name('projects.update');
     Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
